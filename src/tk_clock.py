536f9aace4a2139685ed10aaf1a4f276356cbb3b
Old Song <oldpopsong@qq.com>
2020-03-10 15:04:10 +0800

    文字部分基本完成。控件大小乱变还得研究下怎么解决

--- src/tk_clock.py
+++ src/tk_clock.py
@@ -67,9 +67,9 @@ next_set_lbl = ttk.Label(content, text="Next Sunset: ")
 next_set_lbl.grid(column=0, row=2)
 
 # Today/Tomorrow indication
-today_tomorrow_var = StringVar()
-today_tomorrow_lbl = ttk.Label(content, textvariable=today_tomorrow_var)
-today_tomorrow_lbl.grid(column=1, row=2)
+today_tomorrow_set_var = StringVar()
+today_tomorrow_set_lbl = ttk.Label(content, textvariable=today_tomorrow_set_var)
+today_tomorrow_set_lbl.grid(column=1, row=2)
 
 # show next sunset time
 next_sunset_var = StringVar()
@@ -91,10 +91,10 @@ canvas.grid(column=0, row=3)
 canvas.create_line((0,0,100,100))
 
 # ============== METHODS ==================
-def tick(tz):
-    now_str = datetime.datetime.now(tz).strftime('%c %Z')
-    clock_var.set(datetime.datetime.now(tz).strftime('%c %Z'))
-    clock_lbl.after(200, tick, tz)
+
+def tick():
+    update()
+    clock_lbl.after(simu_tick_ms, tick)
 
 # Calculate standard timezone from longitude, return a timezone.timezone object
 def get_std_timezone(lng):
@@ -108,7 +108,14 @@ def myjulian_to_jd(d_with_tz):
 
 # Julian.from_jd() return naive UTC datetime
 def myjulian_from_jd(j, tz):
-    return julian.from_jd(j).replace(tz=datetime.timezone.utc).replace(tz=tz)
+    return julian.from_jd(j).replace(tzinfo=datetime.timezone.utc).astimezone(tz)
+
+# time interval as string like 'HH:MM' between 2 Julian days
+def myjulian_interval(j1, j2):
+    d = julian.from_jd(j2) - julian.from_jd(j1)  # timedelta object
+    h = d.total_seconds() // 3600
+    m = (d.total_seconds() % 3600) / 60
+    return "%d:%d" % (h, m)
 
 # Get timezone, datetime objects of the noon of the specified local date
 # in local timezone and in UTC
@@ -130,7 +137,49 @@ def get_noons(year, month, day, lat, lon):
     return {'tz': tz, 'd_local': d_local, 'd_utc': d_utc}
 
 # update all the widgets
-def update(info):
+def update():
+    global simu_curr_tick, simu_curr_day
+    if simu_tick_step_j*simu_curr_tick > sun[simu_curr_day]['j_total']:
+        simu_curr_day += 1
+        if simu_curr_day == simulate_days:
+            simu_curr_day = 0
+        simu_curr_tick = 0
+
+    # current Julian day
+    j = sun[simu_curr_day]['j_start'] + simu_curr_tick*simu_tick_step_j
+
+    clock_var.set(myjulian_from_jd(j, noons['tz']).strftime('%c %Z'))
+    if j < sun[simu_curr_day]['j_rise'] or j > sun[simu_curr_day]['j_set']:
+        day_night_var.set('Night')
+    else:
+        day_night_var.set('Day')
+
+    if j < sun[simu_curr_day]['j_rise']:  # before today's sunrise
+        today_tomorrow_var.set('Today')
+        next_sunrise_var.set(myjulian_from_jd(sun[simu_curr_day]['j_rise'], noons['tz']).
+                strftime('%H:%M'))
+        sunrise_countdown_var.set(myjulian_interval(j, sun[simu_curr_day]['j_rise']))
+        today_tomorrow_set_var.set('Today')
+        next_sunset_var.set(myjulian_from_jd(sun[simu_curr_day]['j_set'], noons['tz']).
+                strftime('%H:%M'))
+        sunset_countdown_var.set(myjulian_interval(j, sun[simu_curr_day]['j_set']))
+    else:  # after today's sunrise
+        today_tomorrow_var.set('Tomorrow')
+        next_sunrise_var.set(myjulian_from_jd(sun[simu_curr_day + 1]['j_rise'], noons['tz']).
+                strftime('%H:%M'))
+        sunrise_countdown_var.set(myjulian_interval(j, sun[simu_curr_day + 1]['j_rise']))
+        if j < sun[simu_curr_day]['j_set']:  # still before today's sunset
+            today_tomorrow_set_var.set('Today')
+            next_sunset_var.set(myjulian_from_jd(sun[simu_curr_day]['j_set'], noons['tz']).
+                    strftime('%H:%M'))
+            sunset_countdown_var.set(myjulian_interval(j, sun[simu_curr_day]['j_set']))
+        else: # after today's sunset
+            today_tomorrow_set_var.set('Tomorrow')
+            next_sunset_var.set(myjulian_from_jd(sun[simu_curr_day + 1]['j_set'], noons['tz']).
+                    strftime('%H:%M'))
+            sunset_countdown_var.set(myjulian_interval(j, sun[simu_curr_day + 1]['j_set']))
+
+    simu_curr_tick += 1
 
 # XXX imported from sunclock.py, just for reference
 # Calculate, result as a tuple:
@@ -156,30 +205,42 @@ def update(info):
 # ============= main =============
  
 simu_pre_rise_hour = 2  # we simulate 2 hours pre sunrise
+simu_pre_rise_j = simu_pre_rise_hour / 24  # in Julian day
 simu_aft_set_hour = 2  # simulate 2 hours after sunset, then jump to next sunrise - pre hour
+simu_aft_set_j = simu_aft_set_hour / 24  # in Julian day
 simu_tick_ms = 200  # microseconds of simulating tick
 simu_tick_step_minutes = 5  # simulating minutes per tick
+simu_tick_step_j = simu_tick_step_minutes / 1440  # Julian day per tick
 
 simu_curr_day = 0  # the day we are simulating
 simu_curr_tick = 0 # the tick number at the day we are simulating
 
-if __name__ == '__main__':
-    year, month, day = 2020, 3, 9  # simulation start date
-    simulate_days = 3
-    lat, lon = 51, 0.1   # London
-    lat, lon = 39, 170    # 
-    lat, lon = 39, 179    # 
-    lat, lon = 39, 116    # Beijing
-    lat, lon = 34, -118   # Los Angles
+year, month, day = 2020, 3, 9  # simulation start date
+simulate_days = 3
+lat, lon = 51, 0.1   # London
+lat, lon = 39, 170    #
+lat, lon = 39, 179    #
+lat, lon = 39, 116    # Beijing
+lat, lon = 34, -118   # Los Angles
 
+if __name__ == '__main__':
     noons = get_noons(year, month, day, lat, lon)  # noon of the simulation start day
+    print(noons)
     # calcuate number of days since Jan 1st, 2000 12:00 UTC
-    n = round(julian.to_jd(noons['d_utc']) - julian.to_jd(datetime.datetime(year, month, day, hour=12)))
+    n = round(julian.to_jd(noons['d_utc']) - julian.to_jd(datetime.datetime(2000, 1, 1, hour=12)))
     sun = []
     for i in range(simulate_days + 1):   # calculate sunrise/sunset data
-        sun.append(sun_rise_set(n + i, lon, lat))
+        s = sun_rise_set(n + i, lon, lat)
+        print("j_transit: ", s[0], myjulian_from_jd(s[0], noons['tz']).strftime('%c %Z'), flush=True)
+        sun.append({
+            'j_rise': s[0] - s[1],  # sunrise time in Julian day
+            'j_set': s[0] + s[1],   # sunset time in Julian day
+            'j_start': s[0] - s[1] - simu_pre_rise_j,  # simulate start time in Julian day
+            'j_stop': s[0] + s[1] + simu_aft_set_j, # simulate stop time in Julian day
+            'j_total': 2 * s[1] + simu_pre_rise_j + simu_aft_set_j  # time to simulate at this day
+            })
 
     location_var.set("Lat: %s, Lon: %s" % (lat, lon))
-    tick(noons['tz'])
+    tick()
     root.mainloop()
 
